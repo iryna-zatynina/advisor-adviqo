37fe76dabf92334b0d643edf30eb13c064ac6344
iryna-zatynina <ira.zat1997@gmail.com>
2023-06-21 14:00:43 +0300

    add filtering to backend

--- routes/advisor.route.js
+++ routes/advisor.route.js
@@ -1,27 +1,63 @@
-const { Router } = require('express')
-const router = Router()
-const Advisor = require("../models/Advisor")
+const { Router } = require("express");
+const router = Router();
+const Advisor = require("../models/Advisor");
 
-router.post('/add/advisors', async (req, res) => {
-    try {
-        const {icon, fullName, reviews, language, status, onSiteSince} = req.body
-        const advisor = new Advisor({icon, fullName, reviews, language, status, onSiteSince})
-    
-        await advisor.save()
+router.post("/add/advisors", async (req, res) => {
+  try {
+    const { icon, fullName, reviews, language, status, onSiteSince } = req.body;
+    const advisor = new Advisor({
+      icon,
+      fullName,
+      reviews,
+      language,
+      status,
+      onSiteSince,
+    });
 
-        res.status(201).json("advisor is added")
-    } catch (error) {
-        console.log(error)
+    await advisor.save();
+
+    res.status(201).json("advisor is added");
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// router.get('/advisors', async (req, res) => {
+//     try {
+//         const advisors = await Advisor.find()
+//         setTimeout(() => {
+//             res.json(advisors);
+
+//         }, 1000);
+//     } catch (error) {
+//         console.log(error)
+//     }
+// })
+router.get("/advisors", async (req, res) => {
+  try {
+    const { limit, language, status } = req.query;
+    const filters = {};
+    if (language) {
+      filters.language = language;
     }
-})
-
-router.get('/advisors', async (req, res) => {
-    try {
-        const advisors = await Advisor.find()
-        res.json(advisors)
-    } catch (error) {
-        console.log(error)
+    if (status) {
+      filters.status = status;
     }
-})
 
-module.exports = router
+    const advisorsQuery = Advisor.find(filters);
+    const advisorsCountQuery = Advisor.countDocuments(filters);
+
+    const [advisors, advisorsCount] = await Promise.all([
+      advisorsQuery.limit(limit),
+      advisorsCountQuery
+    ]);
+
+    setTimeout(() => {
+      res.json({ advisors, count: advisorsCount });
+    }, 1000);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+module.exports = router;
